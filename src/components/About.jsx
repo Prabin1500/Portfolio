4da4c08b7f65ccba11c6e6001ed783144bf47e0d
prabind <prabin.dhakal@metropolia.fi>
2025-05-27 17:38:53 +0300

    Home and about me section updateed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,59 +1,116 @@
 import React from 'react'
+import { motion } from "motion/react"
 import { FaCss3, FaGithub, FaHtml5, FaJsSquare, FaNodeJs, FaReact } from 'react-icons/fa';
+import code_icon from '../assets/code-icon.png';
+import code_icon_dark from '../assets/code-icon-dark.png';
 
-const About = () => {
-    return (
-      <div name='about' className='w-full bg-[#0a192f] text-gray-300'>
-        <div className='flex flex-col  pb-80 px-12 py-24 justify-center items-center w-full h-full'>
-          <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-            <div className='sm:text-left pb-8 pl-4'>
-              <p className='text-6xl font-mono inline text-[#08FDD8]'>
-                About Me
-              </p>
-            </div>
-            <div></div>
-            </div>
-            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-              <div className='font-mono'>
-                <p>I am very passionate and ambitious front-end developer looking for a role in 
-                  established IT company with the opportunity to work with latest technologies on challenging 
-                  and diverse projects.
-                </p>
-                <br />
-                <p>Coding, logical thinking and reading are the things that I like most.</p>
+const About = ({isDarkMode}) => {
+  return (
+    <motion.div 
+      initial={{opacity: 0}}
+      whileInView={{opacity:1}}
+      transition={{duration: 1}}
+      id='about' 
+      className='w-full px-4 md:px-[12%] py-10 mb-32 scroll-mt-20 flex flex-col justify-center'>
+      <motion.h2 
+        initial={{opacity: 0, y:-20}}
+        whileInView={{opacity:1, y:0}}
+        transition={{duration: 0.5, delay:0.3}}
+        className='text-center text-4xl md:text-6xl font-Ovo mx-auto border-b-4 border-b-orange-300 dark:text-[#08FDD8] dark:border-b-[#08FDD8]'>
+          About Me
+      </motion.h2>
+      <motion.div 
+        initial={{opacity: 0}}
+        whileInView={{opacity:1}}
+        transition={{duration: 0.8}}
+        className='flex w-full flex-col lg:flex-row items-center justify-center gap-10 md:gap-20 sm:my-10'>
+
+        {/* Text Content - comes first in mobile */}
+        <motion.div 
+          initial={{opacity: 0}}
+          whileInView={{opacity:1}}
+          transition={{duration: 0.6, delay: 0.5}}
+          className='w-full lg:w-1/2 order-2 lg:order-1'>
+            <p className='mb-5 font-Ovo text-lg md:text-xl'>
+                I'm Prabin Dhakal, a developer with a background in mechanical engineering and a strong passion 
+                for technology and software development. After transitioning into IT, I've worked on a wide range 
+                of academic and personal projects involving web development, mobile apps, and database systems.
+            </p>
+            <p className='mb-5 font-Ovo text-lg md:text-xl'>
+                Currently, I'm completing my Bachelor's degree in Information Technology at Metropolia University 
+                of Applied Sciences. I've built applications using Java, Kotlin, React, JavaScript, HTML/CSS, SQL, 
+                and tools like Firebase, Node.js, and Git. During my internship at Kivapix, I contributed to frontend 
+                development using React and TypeScript, and also collaborated with a startup team to bring real ideas to life.
+            </p>
+            <p className='mb-5 font-Ovo text-lg md:text-xl'>
+                I've also worked in a professional lab environment with Microsoft HoloLens, which sharpened my teamwork and 
+                quality control skills. I'm always looking to grow, whether it's through learning new technologies or 
+                building products that make a difference.
+            </p>
+            <p className='mb-8 font-Ovo text-lg md:text-xl'>
+                I'm excited to find opportunities where I can contribute to meaningful projects, keep learning, and grow 
+                as a developer.
+            </p>
+            <motion.ul 
+              initial={{opacity: 0}}
+              whileInView={{opacity:1}}
+              transition={{duration: 0.8, delay: 0.8}}
+              className='grid grid-cols-1 sm:grid-cols-2 gap-5 md:gap-10'>
+                <motion.li 
+                    initial={{ x:-110 , opacity: 0 }}
+                    whileInView={{x:0, opacity:1}}
+                    transition={{duration: 0.5, delay: 0.8}}
+                    className='border-[0.55px] border-gray-400 rounded-xl p-4 md:p-6 cursor-pointer 
+                      hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black
+                      dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
+                      >
+                    <img className='w-6 md:w-7 mt-2 md:mt-3' src={isDarkMode ? code_icon_dark : code_icon} alt="title" />
+                    <h3 className='my-2 md:my-4 font-semibold text-gray-700 dark:text-white text-sm md:text-base'>Languages</h3>
+                    <p className='text-gray-600 text-xs md:text-sm dark:text-white/80'>HTML, CSS, JavaScript React Js, Next Js</p>
+                </motion.li>
+                <motion.li 
+                    initial={{ x:110 , opacity: 0 }}
+                    whileInView={{x:0, opacity:1}}
+                    transition={{duration: 0.5, delay: 0.8}}
+                    className='border-[0.55px] border-gray-400 rounded-xl p-4 md:p-6 cursor-pointer 
+                      hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black
+                      dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
+                      >
+                    <img className='w-6 md:w-7 mt-2 md:mt-3' src={isDarkMode ? code_icon_dark : code_icon} alt="title" />
+                    <h3 className='my-2 md:my-4 font-semibold text-gray-700 dark:text-white text-sm md:text-base'>Projects</h3>
+                    <p className='text-gray-600 text-xs md:text-sm dark:text-white/80'>Built more than 5 projects</p>
+                </motion.li>
+            </motion.ul>
+        </motion.div>
+
+        {/* Cube Spinner - comes second in mobile */}
+        <div className='w-full lg:w-1/2 flex items-center justify-center order-1 mt-20 lg:mt-1 lg:order-2'>
+          <div className='stage-cube-cont w-[250px] h-[250px] md:w-[400px] md:h-[400px] relative'>
+            <div className='cubespinner'>
+              <div className='face1'>
+                <FaNodeJs color='#DD0031' size={60} className='w-10 h-10 md:w-16 md:h-16' />
               </div>
-              <div className='stage-cube-cont'>
-                <div className='cubespinner'>
-                  <div className='face1'>
-                    <FaNodeJs color='#DD0031' />
-                  </div>
-                  
-                  <div className='face2'>
-                    <FaHtml5 color='#F06529' />
-                  </div>
-
-                  <div className='face3'>
-                    <FaGithub color='#EC4D28' />
-                  </div>
-
-                  <div className='face4'>
-                    <FaJsSquare color='#EFD81D'/>
-                  </div>
-
-                  <div className='face5'>
-                    <FaCss3 color='#28a4d9'/>
-                  </div>
-
-                  <div className='face6'>
-                    <FaReact color='#5ed4f4'/>
-                  </div>
-
-                </div>
+              <div className='face2'>
+                <FaHtml5 color='#F06529' size={60} className='w-10 h-10 md:w-16 md:h-16' />
+              </div>
+              <div className='face3'>
+                <FaGithub color='#EC4D28' size={60} className='w-10 h-10 md:w-16 md:h-16' />
+              </div>
+              <div className='face4'>
+                <FaJsSquare color='#EFD81D' size={60} className='w-10 h-10 md:w-16 md:h-16'/>
+              </div>
+              <div className='face5'>
+                <FaCss3 color='#28a4d9' size={60} className='w-10 h-10 md:w-16 md:h-16'/>
+              </div>
+              <div className='face6'>
+                <FaReact color='#5ed4f4' size={60} className='w-10 h-10 md:w-16 md:h-16'/>
               </div>
             </div>
+          </div>
         </div>
-      </div>
-    );
-  };
+      </motion.div>
+    </motion.div>
+  )
+}
 
 export default About
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,73 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
-import {Link} from 'react-scroll'
+import userImage from '../assets/UserImage.jpg'
+import handIcon from '../assets/hand-icon.png'
+import { motion } from "motion/react"
+import downloadIcon from '../assets/download-icon.png'
+import rightArrowWhite from '../assets/right-arrow-white.png'
 
 const Home = () => {
   return (
-    <div name = 'home' className='w-full h-screen bg-[#0a192f]'>
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen scroll-mt-20 flex flex-col items-center justify-center gap-4'>
+        <motion.div
+        initial={{scale: 0}}
+        whileInView={{scale:1}}
+        transition={{duration:0.8, type: 'spring', stiffness: 100}}
+        >
+        <img src={userImage} alt='MyImage'className='rounded-full w-32'/>
+        </motion.div>
+        <motion.h3 
+            initial={{y: -20, opacity:0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{duration:0.6, delay: 0.3}}
+            className='flex items-end gap-2 text-xl sm:text-xl md:text-3xl sm:mb-2 font-Ovo'>
+            Hi! I am Prabin 
+            <img src={handIcon} alt='HandIcon' className='w-6' />
+        </motion.h3>
+        <motion.h1 
+            initial={{y: -30, opacity:0}}
+            whileInView={{y: 0, opacity: 1}}
+            transition={{duration:0.8, delay: 0.5}}
+            className='text-2xl sm:text-3xl lg:text-[66px] font-Ovo mb-2'>
+            Full Stack Developer
+        </motion.h1>
+        <motion.p 
+            initial={{opacity:0}}
+            whileInView={{opacity: 1}}
+            transition={{duration:0.6, delay: 0.7}}
+            className='max-w-3xl mx-auto font-Ovo text-base sm:text-lg md:text-xl'>
+            I design and build responsive, user-focused web and mobile applications. 
+            With hands-on experience in frontend and backend development, I bring ideas 
+            to life using technologies like React, JavaScript, Node.js, Java, Kotlin, and SQL.
+        </motion.p>
+        <motion.p 
+            initial={{opacity:0}}
+            whileInView={{opacity: 1}}
+            transition={{duration:0.6, delay: 0.7}}
+            className='max-w-3xl mx-auto font-Ovo text-base sm:text-lg md:text-xl'>
+            💼 Looking for opportunities in full-stack or software development.
+        </motion.p>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+            <motion.a 
+                initial={{y: 30, opacity:0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration:0.6, delay: 1}}
+                href='#contact'
+                className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2
+                dark:bg-transparent'
+            >
+                Contact 
+                <img src={rightArrowWhite} alt='' className='w-4' /> 
+            </motion.a>
 
-        {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-12 flex flex-col justify-center h-full'>
-            <h1 className='text-5xl sm:text-6xl font-mono my-4 text-[#ccd6f6]'>Hi,</h1>
-            <h1 className='text-5xl sm:text-6xl font-mono mb-4 text-[#ccd6f6]'>I'm Prabin,</h1>
-            <h2 className='text-5xl sm:text-6xl font-mono text-[#ccd6f6]'>Full Stack Developer</h2>
-            <p className='text-[#8892b0] font-mono py-4 max-w-[700px]'>Worked collaboratively with other students in group projects 
-            as well as worked on individual coding challanges.
-            Focused on building responsive full-stack web applications.
-            </p>
-            <div>
-                <Link className='hover:font-thin' to="about" smooth={true} duration={500}>
-                  <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                  View Work
-                  <span className='group-hover:rotate-90 duration-300'>
-                  <HiArrowNarrowRight className='ml-3 ' />
-                  </span>
-                  </button>
-                </Link>
-                
-            </div>
+            <motion.a 
+                initial={{y: 30, opacity:0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration:0.6, delay: 1}}
+                href='/PrabinDhakalResume.pdf' download 
+                className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'
+            >
+                My Resume 
+                <img className='w-4' src={downloadIcon} alt="Download Icon" />
+            </motion.a>
         </div>
     </div>
   )
